4053a540f195ddbd6a2c1f1d8f6c824772efcf5f
Tinotenda Nyashanau <tinotendanyash@gmail.com>
2022-11-19 19:57:22 +0100

    second stage

--- main.py
+++ main.py
@@ -1,14 +1,39 @@
 from fastapi import FastAPI, BackgroundTasks
 from pydantic import BaseModel, validator 
+import task
+
+app = FastAPI()
+
+langueges = ["English" , "French","German","Romanian"]
+
+class Translation(BaseModel):
+    text: str
+    base_lang:str
+    final_lang: str 
+
+    @validator('base_lung','final_lang')
+    def valid_lang(cls,lang):
+        if lang not in langueges:
+            raise ValueError("Invalid languege")
+        return lang
 
 ## route 1
 ## Test if evrything is working 
 ## {"mmessage": "Hellow world"}
+@app.get("/")
+def get_root():
+    return {"mmessage": "Hellow world"} 
 
 ## route 2: / translate 
 ## Take in translation request , and store in the db 
 ## Return a translation id 
 
+@app.post("/translate")
+def post_translation(t:Translation , background_task:BackgroundTasks):
+    ## store the translation
+    ##run
+    
+    pass
 ## route 3 : / results
 ## Take in a translation id 
 ## return the translated id 
--- task.py
+++ task.py
@@ -1,8 +1,21 @@
+from transformers import T5Tokenizer , T5orConditionalGeneration 
+from models import TranslationModel
+
+tokenizer = T5Tokenizer.from_pretrained("t5-small", model_max_length=512)
+translator = T5orConditionalGeneration.from_pretrained("t5-small")
 ## store_translation 
 ## Translation request and save it to the db 
+def store_translation(t):
+    model = TanslationModel(text =t.text , base_lang = t.base-lang , final_lang = t.final_lang)
+    model.save()
+    return model.id
 
 ## run_translation 
 ## Run a pretrained deep learning model 
+def run_translation(t_id:int):
+   model = TranslationModel.get_by_id(t_id)
+   prefex = f"tranlate {model.base_lang} to {model.final_lang}: hellow world"
+
 
 ##find_translation
 ##retrieve a translation from db 
